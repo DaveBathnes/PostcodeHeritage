b6444b43980ffd4fa2e7665db8549d53be573aa1
Dave Rowe <Dave.Rowe@bristol.gov.uk>
2019-08-09 12:24:03 +0100

    Adding new areas and distance customisation

--- site.js
+++ site.js
@@ -17,43 +17,111 @@ $(function () {
                 callback(data);
             },
             error: function (jqXHR, textStatus, errorThrown) {
-
+                // 
             }
         });
     };
 
+    var getConservationArea = function (bbox) {
+        getGISLayer('16', bbox, function (data) {
+            $('#col_conservationareas').hide();
+            $('#col_conservationareas').empty();
+            $('#col_conservationareas').append('<h5>Conservation Areas</h5>');
+            if (data && data.features) {
+                if (data.features.length === 0) $('#col_conservationareas').append('<p class="lead">None found</p>');
+                for (x = 0; x < data.features.length; x++) {
+                    if (x < 3) {
+                        $('#col_conservationareas').append('<p class="lead">' + data.features[x].attributes.LOCALITY + ': ' + '<a href="' + data.features[x].attributes.PDF + '" target="_blank">PDF</a></p>');
+                    }
+                }
+            }
+            $('#col_conservationareas').show();
+        });
+    };
 
-    var getBuildingData = function (bbox) {
+    var getListedBuilding = function (bbox) {
         getGISLayer('11', bbox, function (data) {
-            $('#col_buildings').hide();
-            $('#col_buildings').empty();
-            $('#col_buildings').append('<h5>Listed buildings</h5>');
+            $('#col_listedbuildings').hide();
+            $('#col_listedbuildings').empty();
+            $('#col_listedbuildings').append('<h5>Listed buildings</h5>');
             if (data && data.features) {
+                if (data.features.length === 0) $('#col_listedbuildings').append('<p class="lead">None found</p>');
                 for (x = 0; x < data.features.length; x++) {
                     if (x < 3) {
-                        $('#col_buildings').append('<p class="lead">' + data.features[x].attributes.ALT_LIST_NAME + '</p>');
+                        $('#col_listedbuildings').append('<p class="lead">' + data.features[x].attributes.ALT_LIST_NAME + '</p>');
                     }
                 }
             }
-
-            $('#col_buildings').show();
+            $('#col_listedbuildings').show();
         });
     };
 
-    var getMonumentsData = function (bbox) {
+    var getScheduledMonuments = function (bbox) {
         getGISLayer('9', bbox, function (data) {
-            $('#col_monuments').hide();
-            $('#col_monuments').empty();
-            $('#col_monuments').append('<h5>Monuments</h5>');
+            $('#col_scheduledmonuments').hide();
+            $('#col_scheduledmonuments').empty();
+            $('#col_scheduledmonuments').append('<h5>Scheduled Monuments</h5>');
             if (data && data.features) {
+                if (data.features.length === 0) $('#col_scheduledmonuments').append('<p class="lead">None found</p>');
                 for (x = 0; x < data.features.length; x++) {
                     if (x < 3) {
-                        $('#col_monuments').append('<p class="lead">' + data.features[x].attributes.KNOWN_AS + ' ' + data.features[x].attributes.PERIOD_TEXT + '</p>');
+                        $('#col_scheduledmonuments').append('<p class="lead">' + data.features[x].attributes.KNOWN_AS + ' ' + data.features[x].attributes.PERIOD_TEXT + '</p>');
                     }
                 }
             }
+            $('#col_scheduledmonuments').show();
+        });
+    };
 
-            $('#col_monuments').show();
+    var getHistoricParksAndGardens = function (bbox) {
+        getGISLayer('14', bbox, function (data) {
+            $('#col_historicparksandgardens').hide();
+            $('#col_historicparksandgardens').empty();
+            $('#col_historicparksandgardens').append('<h5>Historic Parks and Gardens</h5>');
+            if (data && data.features) {
+                if (data.features.length === 0) $('#col_historicparksandgardens').append('<p class="lead">None found</p>');
+                for (x = 0; x < data.features.length; x++) {
+                    if (x < 3) {
+                        $('#col_historicparksandgardens').append('<p class="lead">' + data.features[x].attributes.NAME + ': ' + data.features[x].attributes.DESCRIPTION + '</p>');
+                    }
+                }
+            }
+            $('#col_historicparksandgardens').show();
+        });
+    };
+
+
+    var getRegisteredHistoricParks = function (bbox) {
+        getGISLayer('15', bbox, function (data) {
+            $('#col_registeredhistoricparks').hide();
+            $('#col_registeredhistoricparks').empty();
+            $('#col_registeredhistoricparks').append('<h5>Registered Historic Parks</h5>');
+            if (data && data.features) {
+                if (data.features.length === 0) $('#col_registeredhistoricparks').append('<p class="lead">None found</p>');
+                for (x = 0; x < data.features.length; x++) {
+                    if (x < 3) {
+                        $('#col_registeredhistoricparks').append('<p class="lead">Grade ' + data.features[x].attributes.GRADE + ': ' +  data.features[x].attributes.NAME + '</p>');
+                    }
+                }
+            }
+            $('#col_registeredhistoricparks').show();
+        });
+    };
+
+    var getLocalList = function (bbox) {
+        getGISLayer('31', bbox, function (data) {
+            $('#col_locallist').hide();
+            $('#col_locallist').empty();
+            $('#col_locallist').append('<h5>Local List</h5>');
+            if (data && data.features) {
+                if (data.features.length === 0) $('#col_locallist').append('<p class="lead">None found</p>');
+                for (x = 0; x < data.features.length; x++) {
+                    if (x < 3) {
+                        $('#col_locallist').append('<p class="lead">' + data.features[x].attributes.ALT_LIST_NAME + '</p>');
+                    }
+                }
+            }
+            $('#col_locallist').show();
         });
     };
 
@@ -61,6 +129,7 @@ $(function () {
     $('#btn_search').on('click', function () {
         // Get the postcode
         var postcode = $('#txt_postcode').val();
+        var metres = $('#sel_distance').val();
         $.ajax({
             url: 'https://api.postcodes.io/postcodes/' + encodeURI(postcode),
             dataType: 'json',
@@ -69,11 +138,16 @@ $(function () {
 
                 // create our bounding box
                 var point = turf.point([data.result.longitude, data.result.latitude]);
-                var buffered = turf.buffer(point, 500, { units: 'metres' });
+                var buffered = turf.buffer(point, metres, { units: 'metres' });
                 var bbox = turf.bbox(buffered);
 
-                getBuildingData(bbox);
-                getMonumentsData(bbox);
+                getConservationArea(bbox);
+                getListedBuilding(bbox);
+                getScheduledMonuments(bbox);
+                getHistoricParksAndGardens(bbox);
+                getRegisteredHistoricParks(bbox);
+                getLocalList(bbox);
+
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 // Invalid postcode
